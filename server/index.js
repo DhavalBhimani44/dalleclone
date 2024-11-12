870728526f4918ba873d5d2faa1c03ea5b142414
Dhaval Bhimani <dhavalbhimani14@gmail.com>
2024-11-12 11:39:11 +0530

    body parser changes(payload error)

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import AuthRouter from './routes/AuthRouter.js';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import AuthRouter from "./routes/AuthRouter.js";
 
-import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js';
-import profileRoutes from './routes/profileRouter.js';
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+import profileRoutes from "./routes/profileRouter.js";
 
 dotenv.config();
 
@@ -16,29 +16,29 @@ const app = express();
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
+    app.listen(8080, () => console.log("Server started on port 8080"));
   } catch (error) {
     console.log(error);
   }
 };
 startServer();
 
-app.use(bodyParser.json());
-app.use(cors(
-  {
-    origin: 'https://piclab-flexi.vercel.app',
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Authorization'
-  }
-));
+app.use(bodyParser.json({ limit: "300mb" }));
+app.use(
+  cors({
+    origin: "https://piclab-flexi.vercel.app",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
-app.use('/auth', AuthRouter);
-app.use('/api/v1', profileRoutes); 
-app.get('/', async (req, res) => {
+app.use("/auth", AuthRouter);
+app.use("/api/v1", profileRoutes);
+app.get("/", async (req, res) => {
   res.status(200).json({
-    message: 'Hello from DALL.E!',
+    message: "Hello from DALL.E!",
   });
 });
