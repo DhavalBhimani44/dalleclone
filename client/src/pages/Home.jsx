c64824134810e0b0815e38b675ba260dee16ccd4
Dhaval Bhimani <dhavalbhimani14@gmail.com>
2024-11-12 11:48:53 +0530

    removed no cors

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Card, FormField, Loader } from '../components';
+import { Card, FormField, Loader } from "../components";
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
-    return (
-      data.map((post) => <Card key={post._id} {...post} />)
-    );
+    return data.map((post) => <Card key={post._id} {...post} />);
   }
 
   return (
@@ -18,7 +16,7 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
 
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState(null);
 
@@ -26,13 +24,15 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dalleclone-a8np.onrender.com/api/v1/post', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        mode: 'no-cors',
-      });
+      const response = await fetch(
+        "https://dalleclone-a8np.onrender.com/api/v1/post",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
@@ -55,18 +55,27 @@ const Home = () => {
 
     setSearchTimeout(
       setTimeout(() => {
-        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        const searchResult = allPosts.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
         setSearchedResults(searchResult);
-      }, 500),
+      }, 500)
     );
   };
 
   return (
     <section className="max-w-7xl mx-auto">
-      <div className='bg-[#0C0C0C] px-6 py-4 rounded-xl'>
+      <div className="bg-[#0C0C0C] px-6 py-4 rounded-xl">
         <div>
-          <h1 className="font-extrabold text-[#E0E0E0] text-[32px]">Spotlight on the Community</h1>
-          <p className="mt-2 text-[#4C4C4C] text-[14px] max-w-[500px]">Explore a captivating gallery of imaginative and visually striking images created by DALL-E AI.</p>
+          <h1 className="font-extrabold text-[#E0E0E0] text-[32px]">
+            Spotlight on the Community
+          </h1>
+          <p className="mt-2 text-[#4C4C4C] text-[14px] max-w-[500px]">
+            Explore a captivating gallery of imaginative and visually striking
+            images created by DALL-E AI.
+          </p>
         </div>
 
         <div className="mt-16 text-[#4C4C4C]">
@@ -77,13 +86,12 @@ const Home = () => {
             placeholder="Search something..."
             value={searchText}
             handleChange={handleSearchChange}
-            style={{ backgroundColor: '#4C4C4C' }}
+            style={{ backgroundColor: "#4C4C4C" }}
           />
         </div>
-
       </div>
 
-      <div className='bg-[#0C0C0C] px-6 py-4 rounded-xl mt-8'>
+      <div className="bg-[#0C0C0C] px-6 py-4 rounded-xl mt-8">
         <div className="mt-10">
           {loading ? (
             <div className="flex justify-center items-center">
@@ -93,7 +101,8 @@ const Home = () => {
             <>
               {searchText && (
                 <h2 className="font-medium text-[#666e75] text-xl mb-3">
-                  Showing Resuls for <span className="text-[#4C4C4C]">{searchText}</span>:
+                  Showing Resuls for{" "}
+                  <span className="text-[#4C4C4C]">{searchText}</span>:
                 </h2>
               )}
               <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
@@ -103,10 +112,7 @@ const Home = () => {
                     title="No Search Results Found"
                   />
                 ) : (
-                  <RenderCards
-                    data={allPosts}
-                    title="No Posts Yet"
-                  />
+                  <RenderCards data={allPosts} title="No Posts Yet" />
                 )}
               </div>
             </>
